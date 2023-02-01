8d60cf50d26cd5caea5abaf536e1e773abe65292
AlexT2052 <alextong2004@gmail.com>
2023-01-31 22:07:16 -0600

    more work that will most definitely not work

--- lib/pulse_sensor.h
+++ lib/pulse_sensor.h
@@ -2,15 +2,28 @@
 #ifndef PULSE_SENSOR_H
 #define PULSE_SENSOR_H
 
-#define TIMER_COUNT 4;
-#define IC_BUF_SIZE 64;
+#define TIMER_COUNT 4
+#define IC_BUF_SIZE 64
+#define MS_IN_A_MINUTE 60000
 
-typedef struct Timer
+typedef struct PulseSensor
 {
-	HAL_timer_typdef* timeR;
+	HAL_timer_typdef* htim;
 	TIM_CHANNEL channel;
 	U16 buffer[IC_BUF_SIZE];
-	bool stopped;
+	U16 averageSpeedTimerTicks = 0;
+	U32 timerPeriodNs = 0;
+	U32 lastDMACheckMs = 0;
+	U32 lastDMAReadValueTimeMs = 0;
+	U16 DMA_lastReadValue = 0;
+	U16 lowSamples = 0;
+	U16 highSamples = 0;
+	U16 ticksPerRev = 0;
+	float conversionRatio = 0;
+	float* resultStoreLocation;
+	bool useVariableSpeedSampling = false;
+	bool stopped = false;
 }
 
-U16 checkTransSpeedDMA(U16* ic1buf, U32* lastDMAReadValueTimeMs, U16 averageSpeedMsPerTick);
+void setupTimerAndStartDMA(HAL_timer_typdef* htim, TIM_CHANNEL channel, U32 timerPeriodNs, U16 lowSamples, U16 highSamples, U16 ticksPerRev, float conversionRatio, float* resultStoreLocation, bool useVariableSpeedSampling);
+U16 checkTransSpeedDMAs();
